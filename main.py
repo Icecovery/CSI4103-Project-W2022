5c781fb540d9010bbd4e2756b43e633bcc450144
Icecovery <Icecovery@foxmail.com>
2022-03-05 15:27:36 -0500

    Release servos when keyboard interrupt

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ SRC_IMG_PATH = "./ImageProcessing/tests/functional_tests/test_images/Test_image.
 LETTER_PAPER = Paper(279, 216, -50, 140)
 
 def main():
-    real_space_segments = image_process(SRC_IMG_PATH, LETTER_PAPER, False)    
-    angle_sets = lines_to_angles(real_space_segments)
-    controller = Controller()
-    controller.draw(angle_sets)
+	real_space_segments = image_process(SRC_IMG_PATH, LETTER_PAPER, False)    
+	angle_sets = lines_to_angles(real_space_segments)
+	controller = Controller()
+	try:
+		controller.draw(angle_sets)
+	except KeyboardInterrupt:
+		print("Keyboard interrupt detected, releasing servos...")
+		controller.clean_up()
+		return
 
 if __name__ == "__main__":
-    main()
+	main()
