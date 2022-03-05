28afbe283f2f3d031a615c6186b92b5f80352de1
cyclexit <cyclexit@outlook.com>
2022-03-04 22:19:41 -0500

    Partially implement CoordinatesToAnglesConverter

--- HardwareControl/coordinate_converter.py
+++ HardwareControl/coordinate_converter.py
@@ -1,12 +1,21 @@
 import math
 
+class CoordinatesToAnglesConverter:
+    '''
+        This class is used to convert the coordinates in path.csv to the angles
+        of the servos. Use inverse kinematics to do the converting.
+    '''
+
+    def __init__(self):
+        pass
+
 def coordinate_to_angle(x, y, la, lb):
-	ac = math.sqrt(x ** 2 + y ** 2)
-	bac = math.acos((la ** 2 + ac ** 2 - lb ** 2) / (2.0 * la * ac))
-	yac = math.asin(x / ac)
-	yab = (math.pi - yac - bac) if y > 0 else (yac - bac)
+    ac = math.sqrt(x ** 2 + y ** 2)
+    bac = math.acos((la ** 2 + ac ** 2 - lb ** 2) / (2.0 * la * ac))
+    yac = math.asin(x / ac)
+    yab = (math.pi - yac - bac) if y > 0 else (yac - bac)
 
-	angleA = yab
-	angleB = math.acos((la ** 2 + lb ** 2 - ac ** 2) / (2.0 * la * lb))
+    angleA = yab
+    angleB = math.acos((la ** 2 + lb ** 2 - ac ** 2) / (2.0 * la * lb))
 
-	return (angleA, angleB)
+    return (angleA, angleB)
