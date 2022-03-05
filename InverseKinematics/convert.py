37ecb9c0acf62173d7366f6a423a67855b554fd1
cyclexit <cyclexit@outlook.com>
2022-03-05 11:36:46 -0500

    Fully implement the InverseKinematics module

---
+++ InverseKinematics/convert.py
@@ -0,0 +1,37 @@
+import math
+
+from .device_abstraction import *
+
+def coordinate_to_angle(x, y, la, lb):
+    ac = math.sqrt(x ** 2 + y ** 2)
+    bac = math.acos((la ** 2 + ac ** 2 - lb ** 2) / (2.0 * la * ac))
+    yac = math.asin(x / ac)
+    yab = (math.pi - yac - bac) if y > 0 else (yac - bac)
+
+    angle_a = yab
+    angle_b = math.acos((la ** 2 + lb ** 2 - ac ** 2) / (2.0 * la * lb))
+
+    return (angle_a, angle_b)
+
+def lines_to_angles(lines):
+    '''
+        @brief Convert line segments produced by ImageProcessing module to the angles
+        which are used to control the servos.
+    '''
+    angle_sets = []
+    for line in lines:
+        start, end = line
+        # current angles
+        angle_a1, angle_b1 = coordinate_to_angle(start[0], start[1], ARM_A_LEN, ARM_B_LEN)
+        # next angles
+        angle_a2, angle_b2 = coordinate_to_angle(end[0], end[1], ARM_A_LEN, ARM_B_LEN)
+        
+        angle_set = []
+        angle_set.append(math.degrees(angle_a1) + A_OFFSET)
+        angle_set.append(math.degrees(angle_b1) + B_OFFSET)
+        angle_set.append(math.degrees(angle_a2) + A_OFFSET)
+        angle_set.append(math.degrees(angle_b2) + B_OFFSET)
+
+        angle_sets.append(angle_set)
+
+    return angle_sets
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from ImageProcessing.image_processing import image_process
 from ImageProcessing.paper import Paper
+from ImageProcessing.image_processing import image_process
+from InverseKinematics.convert import lines_to_angles
 
 # constants
 SRC_IMG_PATH = "./ImageProcessing/tests/functional_tests/test_images/Test_image.jpeg"
@@ -9,7 +10,9 @@ LETTER_PAPER = Paper(279, 216, -50, 140)
 
 def main():
     real_space_segments = image_process(SRC_IMG_PATH, LETTER_PAPER, False)
-    print(real_space_segments)
+    # print(real_space_segments)
+    angle_sets = lines_to_angles(real_space_segments)
+    print(angle_sets)
 
 if __name__ == "__main__":
     main()
