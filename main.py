7d3386810c25b07084710144ee6064d6a8d9abb7
cyclexit <cyclexit@outlook.com>
2022-03-08 12:35:53 -0500

    Make src_img_path configurable

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
+import argparse
+
 from ImageProcessing.paper import Paper
 from ImageProcessing.image_processing import image_process
 from InverseKinematics.convert import lines_to_angles
 from HardwareControl.controller import Controller
 
-# constants
-SRC_IMG_PATH = "./ImageProcessing/tests/functional_tests/test_images/Test_image.jpeg"
-
 # some paper pre-sets
 LETTER_PAPER = Paper(279, 216, -50, 140)
 
 def main():
-	real_space_segments = image_process(SRC_IMG_PATH, LETTER_PAPER, False)
+	# parse the command-line args
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-s", "--src_img_path",
+						required=True, dest="src_img_path", action="store", type=str, help="Path to the source image")
+	cmd_args = parser.parse_args()
+
+	real_space_segments = image_process(cmd_args.src_img_path, LETTER_PAPER, False)
 	angle_sets = lines_to_angles(real_space_segments)
 	controller = Controller()
 	try:
