f0f091cc2ad330d720018b3ec3fde4d711aa12f3
cyclexit <cyclexit@outlook.com>
2022-03-30 12:21:43 -0400

    Renaming

--- ImageProcessing/pixel_to_real_space_converter.py
+++ ImageProcessing/pixel_to_real_space_converter.py
@@ -26,17 +26,17 @@ class PixelToRealSpaceConverter:
 			raise AttributeError()
 		new_segments = []
 		
-		pixelSize = (shape[0], shape[1])
+		pixel_size = (shape[0], shape[1])
 
 		# scale image to fit on paper
 		scaling_factor = 1.0
 
-		if pixelSize[0] > pixelSize[1]:  # image is wider
+		if pixel_size[0] > pixel_size[1]:  # image is wider
 			# use width to calculate scaling factor
-			scaling_factor = realSize[0] / pixelSize[0]
+			scaling_factor = realSize[0] / pixel_size[0]
 		else: # image is taller
 			# use height to calculate scaling factor
-			scaling_factor = realSize[1] / pixelSize[1]
+			scaling_factor = realSize[1] / pixel_size[1]
 
 		for segment in progressbar.progressbar(segments):
 			x1 = segment[0][0] * scaling_factor - rootCoordinate[0]
@@ -67,10 +67,10 @@ class PixelToRealSpaceConverter:
 			for s in range(0, len(new_segments)):
 
 				# remove root offset
-				realPointStart = ((new_segments[s][0][0] + rootCoordinate[0]) * scale, (new_segments[s][0][1] + rootCoordinate[1]) * scale)
-				realPointEnd = ((new_segments[s][1][0] + rootCoordinate[0]) * scale, (new_segments[s][1][1] + rootCoordinate[1]) * scale)
+				real_start = ((new_segments[s][0][0] + rootCoordinate[0]) * scale, (new_segments[s][0][1] + rootCoordinate[1]) * scale)
+				real_end = ((new_segments[s][1][0] + rootCoordinate[0]) * scale, (new_segments[s][1][1] + rootCoordinate[1]) * scale)
 				
-				cv.line(real_size_img, realPointStart, realPointEnd, (255, 255, 255), 1, cv.LINE_AA)
+				cv.line(real_size_img, real_start, real_end, (255, 255, 255), 1, cv.LINE_AA)
 
 			cv.imshow("Real Size", real_size_img)
 
